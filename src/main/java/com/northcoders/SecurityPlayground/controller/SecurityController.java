fd15b795ddf7616f3f3617f0bd4b9d206b8c490f
Emmanuel <asiegbuemmanuelekee@gmail.com>
2024-11-29 16:46:30 +0000

    feat: Retrieved username from authenticated user and added this to application page

--- src/main/java/com/northcoders/SecurityPlayground/controller/SecurityController.java
+++ src/main/java/com/northcoders/SecurityPlayground/controller/SecurityController.java
@@ -1,9 +1,17 @@
 package com.northcoders.SecurityPlayground.controller;
 
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping ("/api/v1")
@@ -14,8 +22,9 @@ public class SecurityController {
         return "Hello";
     }
     @GetMapping("/protected/greeting")
-    public String protectedGreeting() {
-        return "Secure";
+    public String protectedGreeting(@AuthenticationPrincipal OAuth2User oAuth2User) {
+
+        return "Hello " + oAuth2User.getAttribute("name");
     }
 
 }
